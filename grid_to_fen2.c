2b33662832cbcbf469a610bc3f6822670dad6c83
SarahH31 <154449701+SarahH31@users.noreply.github.com>
2023-12-27 14:38:43 +0100

    Add files via upload

---
+++ grid_to_fen2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "grille.h"
+
+char* grid_to_fen(grille_m grille) {
+    char* fen = malloc(10); // Allouez de la mémoire pour le tableau de caractères (FEN est composé d'au plus 10 caractères)
+    int curseur = 0;
+    int compteur = 0;
+
+    for (int i = 0; i < 9; i++) {
+        if (isalpha(grille.cases[i].joueur)) {
+            if (compteur != 0) {
+                // Convertissez le compteur en caractère et stockez-le dans le tableau FEN
+                fen[curseur++] = '0' + compteur;
+                compteur = 0;
+            }
+
+            // Stockez le joueur dans le tableau FEN
+            fen[curseur++] = grille.cases[i].joueur;
+        } else if (grille.cases[i].joueur == '.') {
+            compteur++;
+        }
+    }
+
+    if (compteur != 0) {
+        // Convertissez le compteur final en caractère et stockez-le dans le tableau FEN
+        fen[curseur++] = '0' + compteur;
+    }
+
+    // Ajoutez le caractère de fin de chaîne
+    fen[curseur] = '\0';
+
+    return fen;
+}
+
+int main() {
+    // Initialisez une grille avec des caractères
+    grille_m grille;
+    grille.cases[0].joueur = 'x';
+    grille.cases[1].joueur = 'o';
+    grille.cases[2].joueur = '.';
+    grille.cases[3].joueur = '.';
+    grille.cases[4].joueur = 'x';
+    grille.cases[5].joueur = 'x';
+    grille.cases[6].joueur = 'o';
+    grille.cases[7].joueur = 'o';
+    grille.cases[8].joueur = '.';
+
+    // Appelez la fonction grid_to_fen et imprimez le résultat
+    char* fen_result = grid_to_fen(grille);
+    printf("%s\n", fen_result);
+
+    // N'oubliez pas de libérer la mémoire allouée
+    free(fen_result);
+
+    return 0;
+}
+
