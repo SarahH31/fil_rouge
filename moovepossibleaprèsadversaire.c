23518ec672688a29c6d7aeda1e89aa7bd41e06fc
ClaraM2003 <154450278+ClaraM2003@users.noreply.github.com>
2024-01-09 13:07:02 +0100

    Create moovepossibleaprèsadversaire.c

---
+++ moovepossibleaprèsadversaire.c
@@ -0,0 +1,27 @@
+int détection_coup_jouable (indice, position sm){
+
+##recup indice pour verif si partie finie, si pas fini min max appliqué à la grille de cet indice, sinon choix parmi tt
+
+if partie finie sm.grilles[indice]='n'{
+	
+  ###appliquer minmax à la grille indice
+}
+
+if partie finie sm.grilles[indice]!='n'{
+	int a=i;
+	while patie finie sm.grilles[a]!='n'{
+		srand(time(NULL));
+
+    // Générez un nombre aléatoire entre 0 et RAND_MAX
+   		 int randomValue = rand();
+
+    // Mappez la valeur dans la plage de 0 à 8
+    		double a = (double)randomValue / RAND_MAX * 8.0;
+}
+
+		return a;
+
+	
+		#####appliquer le min max à la sm.grilles[a];
+   }
+}
