b3db1dfb89bc2e85347d24661ca9f09185734c80
ClaraM2003 <154450278+ClaraM2003@users.noreply.github.com>
2024-01-09 13:49:48 +0100

    Update P2moovepossibleaprèsadversaire.c

--- P2moovepossibleaprèsadversaire.c
+++ P2moovepossibleaprèsadversaire.c
@@ -2,14 +2,14 @@ int détection_coup_jouable (indice, position sm){
 
 ##recup indice pour verif si partie finie, si pas fini min max appliqué à la grille de cet indice, sinon choix parmi tt
 
-if partie finie sm.grilles[indice]='n'{
+if (partie_finie(sm.grilles[indice]='n')){
 	
-  ###appliquer minmax à sm.grilles[indice]
+  ###appliquer minmax à la grille indice
 }
 
-if partie finie sm.grilles[indice]!='n'{
-	int a=i;
-	while patie finie sm.grilles[a]!='n'{
+if (partie_finie (sm.grilles[indice]!='n')){
+	int a=indice;
+	while(partie_finie( sm.grilles[a]!='n')){
 		srand(time(NULL));
 
     // Générez un nombre aléatoire entre 0 et RAND_MAX
@@ -23,5 +23,4 @@ if partie finie sm.grilles[indice]!='n'{
 
 	
 		#####appliquer le min max à la sm.grilles[a];
-   }
 }
