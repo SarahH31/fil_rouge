e5ee65ee4bf754b83887f9b461cbf66872642eba
ClaraM2003 <154450278+ClaraM2003@users.noreply.github.com>
2024-01-09 12:34:59 +0100

    Update programme2_detectionsuperalignements.c

--- programme2_detectionsuperalignements.c
+++ programme2_detectionsuperalignements.c
@@ -3,7 +3,7 @@ char détection_super_morpion(position sm) {
     char nouvelle_liste[9]={'.','.','.','.','.','.','.','.','.'}   #new_grille_m()??
 
     for (int i = 0; i < 9; i++) {
-        char gagnant = partie_finie(sm.grilles[i]);  #changer le nom de l'argument
+        char gagnant = partie_finie(sm.grilles[i]);  
         if (gagnant == 'x') {
             nouvelle_liste[i] = 'X';
         } else if (gagnant == 'o') {
@@ -12,8 +12,8 @@ char détection_super_morpion(position sm) {
             nouvelle_liste[i] = 'D';
         } else {
             nouvelle_liste[i] = ' ';
-        }
-    }
+        }}
+    
 
     #détection des super alignements
-return partie_finie(nouvelle_liste[9])
+return partie_finie(nouvelle_liste[9])}
